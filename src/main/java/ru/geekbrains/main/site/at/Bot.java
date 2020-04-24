5ee8f4b4e20c0736d3e9e2d3811c29d986728060
Kasaru <artemiy064@bk.ru>
2020-04-24 21:45:18 +0400

    ООП

---
+++ src/main/java/ru/geekbrains/main/site/at/Bot.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.main.site.at;
+
+public class Bot extends Members {
+    public static int JumpH;
+    public static String name;
+    public static int RunD;
+
+
+}
+
+
---
+++ src/main/java/ru/geekbrains/main/site/at/Human.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.main.site.at;
+
+public class Human extends Members {
+    public static int JumpH;
+    public static String name;
+    public static int RunD;
+
+    }
