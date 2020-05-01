3ef015699866da753bd7dd003c7a4c47dc48daca
Kasaru <artemiy064@bk.ru>
2020-05-01 20:18:50 +0400

    Создать массив с набором из 10–20 слов, должны встречаться повторяющиеся. Найти и вывести список уникальных слов, из которых состоит массив. Посчитать, сколько раз встречается каждое слово. Написать простой класс Телефонный Справочник, который хранит список фамилий и телефонных номеров. В него с помощью метода add() можно добавлять записи, а с помощью get() — искать номер телефона по фамилии. Для одной фамилии может быть несколько телефонов (однофамильцы): тогда при запросе такой фамилии должны выводиться все номера. Не надо в телефонную запись добавлять дополнительные поля (имя, отчество, адрес), делать взаимодействие с пользователем через консоль и иначе дополнять задание. Консоль желательно не использовать для ввода, тестировать просто из метода main(), прописывая add() и get().

---
+++ src/main/java/ru/geekbrains/main/site/at/lesson7/Person.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.main.site.at.lesson7;
+
+import ru.geekbrains.main.site.at.Page;
+
+public class Person {
+private String name;
+private int number;
+
+    public Person(String name, int number){
+    this.name=name;
+    this.number=number;
+    }
+
+}
--- src/main/java/ru/geekbrains/main/site/at/lesson7/PhoneBook.java
+++ src/main/java/ru/geekbrains/main/site/at/lesson7/PhoneBook.java
@@ -1,13 +1,19 @@
 package ru.geekbrains.main.site.at.lesson7;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PhoneBook {
+
     public static void main(String[] args) {
-        ArrayList<String> book= new ArrayList<>();
-        book.add("Иванов");
-
-
+        List<Person> book= new ArrayList<>();
+        book.add(new Person("Иванов", 1));
+        book.add(new Person("Cмирнов", 2));
+        book.add(new Person("Сидоров", 4));
+        book.add(new Person("Иванов", 3));
+        
 
 
     }
